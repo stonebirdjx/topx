84e30a14492d0e7842a94f95051cf7868ad0a0a9
stonebird <1245863260@qq.com>
2023-09-09 13:42:27 +0800

    feat:mongodb index

--- biz/dal/mongo.go
+++ biz/dal/mongo.go
@@ -3,6 +3,7 @@ package dal
 import (
 	"context"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -29,5 +30,20 @@ func MongoInit(ctx context.Context, opt MongoOption) error {
 
 	Mongoclient = client
 	TopCol = Mongoclient.Database(opt.DB).Collection(topxColName)
-	return nil
+	return topColInit(ctx)
+}
+
+func topColInit(ctx context.Context) error {
+	index := mongo.IndexModel{
+		// bson.M 没有顺序，必须使用 bson.D
+		Keys: bson.D{
+			{Key: "name", Value: 1},
+			{Key: "service_name", Value: 1},
+			{Key: "version", Value: 1},
+		},
+		Options: options.Index().SetUnique(true),
+	}
+
+	_, err := TopCol.Indexes().CreateOne(ctx, index)
+	return err
 }
