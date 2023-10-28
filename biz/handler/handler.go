a276c4734ada95488b4c90a53d1e93dbd9d2d3fe
stonebird <1245863260@qq.com>
2023-10-28 09:24:26 +0800

    fix:file header comment

--- biz/handler/handler.go
+++ biz/handler/handler.go
@@ -15,6 +15,30 @@
 // only for handler paclage use.
 package handler
 
-import "github.com/stonebirdjx/topx/biz/handler/internal"
+import (
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/common/utils"
+	"github.com/stonebirdjx/topx/biz/handler/internal"
+)
 
 var InitHelper = internal.InitHelper
+
+type errOption struct {
+	statusCode int
+	err        error
+}
+
+func sendError(c *app.RequestContext, opt errOption) {
+	c.JSON(opt.statusCode, utils.H{
+		"message": opt.err.Error(),
+	})
+}
+
+type okOption struct {
+	statusCode int
+	obj        any
+}
+
+func sendOk(c *app.RequestContext, opt okOption) {
+	c.JSON(opt.statusCode, opt.obj)
+}
